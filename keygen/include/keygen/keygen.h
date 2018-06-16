390e397fe0f400ccb130caf359bb9c8513806b0e
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-06-15 22:12:38 -0400

    Finally set up cmake file to satisfaction

--- keygen/include/keygen/keygen.h
+++ keygen/include/keygen/keygen.h
@@ -1,8 +1,52 @@
-﻿// keygen.h : Include file for standard system include files,
-// or project specific include files.
+﻿
+/** ***************************************************************************
+*
+*  Copyright 2018 Jose Fernando Lopez Fernandez - All Rights Reserved
+*  See the file AUTHORS included with the application distribution for
+*  specific information.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following
+*  conditions are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*
+*     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+*     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+*     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+*     MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+*     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+*     SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+*     NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+*     HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+*     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+*     OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+*     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*  ***************************************************************************
+*
+*  @author Jose Fernando Lopez Fernandez
+*  @date 15-June-2018
+*  @brief This is the main keygen driver file
+*
+*  Tasks:
+*      1. @todo Add file details
+*      2. @todo Add file details
+*
+*  **************************************************************************/
 
-#pragma once
+#ifndef KEYGEN_KEYGEN_H_
+#define KEYGEN_KEYGEN_H_
 
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+#endif // KEYGEN_KEYGEN_H_
+
--- keygen/keygen.cpp
+++ keygen/keygen.cpp
@@ -1,45 +1,68 @@
-﻿// keygen.cpp : Defines the entry point for the application.
-//
+﻿
+/** ***************************************************************************
+ *
+ *  Copyright 2018 Jose Fernando Lopez Fernandez - All Rights Reserved
+ *  See the file AUTHORS included with the application distribution for
+ *  specific information.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following
+ *  conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *     MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *     SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *     NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *     HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *     OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  ***************************************************************************
+ *
+ *  @author Jose Fernando Lopez Fernandez
+ *  @date 15-June-2018
+ *  @brief This is the main keygen driver file
+ *
+ *  Tasks:
+ *      1. @todo Add file details
+ *      2. @todo Add file details
+ *
+ *  **************************************************************************/
 
-//#include "keygen.h"
+#include <keygen.h>
 
 #include <cstdlib>
-#include <iostream>
+#include <boost/program_options.hpp>
+#include <gtest/gtest.h>
 
-/**#include <boost/program_options.hpp>*/
-
-// TODO: Incorporate glog
-/**#include <glog/config.h>
-#include <glog/stl_logging.h>
-#include <glog/logging.h>
-
-#include <gtest/gtest.h>*/
-
-/**int returnOne() {
-    return 1;
+namespace Dummy {
+    int ReturnOne() {
+        return 1;
+    }
 }
 
 TEST(GTestConfig, TrivialTest)
 {
-    EXPECT_EQ(1, returnOne());
-}*/
-
+    EXPECT_EQ(1, Dummy::ReturnOne());
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1) {
-        std::cout << "Usage: keygen <opts>\n";
+    testing::InitGoogleTest(&argc, argv);
 
-        return EXIT_FAILURE;
-    }
-
-    for (auto i = 1u; i < argc; i++) {
-        std::cout << argv[i] << '\n';
-    }
-
-    return EXIT_SUCCESS;
-
-    /**testing::InitGoogleTest(&argc, argv);
-
-    return RUN_ALL_TESTS();*/
+    return RUN_ALL_TESTS();
 }
